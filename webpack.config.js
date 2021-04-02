3b8fc237540716ce9f36d523661572eb09b8a087
Jose Chacon <j.daniel.chg@gmail.com>
2021-04-02 15:51:31 -0400

    feat: config webpack in webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"),
+     MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+     entry: {
+          index: "./src/index.js",
+     },
+     output: {
+          filename: "[name].js",
+     },
+     module: {
+          rules: [
+               {
+                    test: /\.js$/i,
+                    exclude: /node_modules/,
+                    use: {
+                         loader: 'babel-loader',
+                    }
+               },
+               {
+                    test: /\.html$/i,
+                    use: [
+                         {
+                              loader: "html-loader",
+                              options: {
+                                   minimize: true,
+                              },
+                         },
+                    ],
+               },
+               {
+                    test: /\.css$/i,
+                    use: [
+                         {
+                              loader: MiniCssExtractPlugin.loader,
+                              options: {
+                                   publicPath: "./",
+                              },
+                         },
+                         "css-loader",
+                    ],
+               },
+               {
+                    test: /\.(jpe?g|png|gif|svg|webp)$/i,
+                    use: ["file-loader?name=assets/[name].[ext]", "image-webpack-loader"],
+               }
+          ],
+     },
+     plugins: [
+          new HtmlWebpackPlugin({
+               template: "./src/index.html",
+               filename: "./index.html",
+          }), 
+          new MiniCssExtractPlugin(),
+     ],
+}
