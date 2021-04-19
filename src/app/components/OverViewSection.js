624fba468ffa27fdd998717a7fc1a3a8942808a4
Jose Chacon <j.daniel.chg@gmail.com>
2021-04-19 19:16:20 -0400

    fix: - Fix bug in OverViewSection component when the movie has no associated poster picture.

--- src/app/components/OverViewSection.js
+++ src/app/components/OverViewSection.js
@@ -37,7 +37,13 @@ export function OverViewSection(props) {
           <div class="details">
                <div class="left">
                     <div class="poster">
-                         <img src="https://image.tmdb.org/t/p/w342${props.poster_path}" alt="${props.title}" class="card__cover">
+                         ${
+                              props.poster_path
+                                   ? `<img src="https://image.tmdb.org/t/p/w342${props.poster_path}" alt="${props.title}" class="card__cover">`
+
+                                   : `<span><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill-rule="evenodd" clip-rule="evenodd" fill="#202124"><path d="M24 22h-24v-20h24v20zm-1-19h-22v18h22v-18zm-1 16h-19l4-7.492 3 3.048 5.013-7.556 6.987 12zm-11.848-2.865l-2.91-2.956-2.574 4.821h15.593l-5.303-9.108-4.806 7.243zm-4.652-11.135c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5zm0 1c.828 0 1.5.672 1.5 1.5s-.672 1.5-1.5 1.5-1.5-.672-1.5-1.5.672-1.5 1.5-1.5z"></path></svg></span>`
+                         }
+                         
                     </div>
                </div>
 
@@ -126,6 +132,21 @@ export function OverViewSection(props) {
                left: 0;
                width: 100%;
                height: 100%;
+          }
+
+          .movie-details__overview .left .poster span {
+               display: flex;
+               align-items: center;
+               justify-content: center;
+               position: absolute;
+               top: 0;
+               left: 0;
+               width: 100%;
+               height: 100%;
+          }
+
+          .movie-details__overview .left .poster svg {
+               vertical-align: middle;
           }
 
           .movie-details__overview .right .overview {
