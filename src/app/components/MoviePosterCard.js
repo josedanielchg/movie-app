4fd5104371ff0308a037aa79672ccadf82f7565d
Jose Chacon <j.daniel.chg@gmail.com>
2021-04-19 22:32:41 -0400

    fix: - Fix bug in MoviePosterCard Component, when the movie doesn't have a poster.

--- src/app/components/MoviePosterCard.js
+++ src/app/components/MoviePosterCard.js
@@ -13,7 +13,12 @@ export function MoviePosterCard(data) {
           $card.insertAdjacentHTML("beforeend", `
                <a href="#/${props.slug}" class="card__link movie-link" title="${props.title}" data-id="${props.id}">
                     <div class="card__cover">
-                         <img src="https://image.tmdb.org/t/p/w342${props.poster_path}" alt="${props.title}" >
+                         ${
+                              props.poster_path
+                              ? `<img src="https://image.tmdb.org/t/p/w342${props.poster_path}" alt="${props.title}" >`
+                              : `<span><svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 24 24" fill-rule="evenodd" clip-rule="evenodd" fill="#20212"><path d="M24 22h-24v-20h24v20zm-1-19h-22v18h22v-18zm-1 16h-19l4-7.492 3 3.048 5.013-7.556 6.987 12zm-11.848-2.865l-2.91-2.956-2.574 4.821h15.593l-5.303-9.108-4.806 7.243zm-4.652-11.135c1.38 0 2.5 1.12 2.5 2.5s-1.12 2.5-2.5 2.5-2.5-1.12-2.5-2.5 1.12-2.5 2.5-2.5zm0 1c.828 0 1.5.672 1.5 1.5s-.672 1.5-1.5 1.5-1.5-.672-1.5-1.5.672-1.5 1.5-1.5z"></path></svg></span>`
+                         }
+                         
                     </div>
                     <h3 class="card__name">${props.title}</h3>
                     <div class="card__rating">
@@ -52,6 +57,8 @@ export function MoviePosterCard(data) {
                position: relative;
                height: 0;
                padding-top: 150.27%;
+               background-color: #ccc;
+               margin-bottom: 0.75rem;
           }
 
           .movie-card .card__cover img {
@@ -70,10 +77,6 @@ export function MoviePosterCard(data) {
 
           .movie-card .card__link {
                overflow: hidden;
-          }
-
-          .movie-card .card__cover {
-               margin-bottom: 0.75rem;
           }
 
          .movie-card .card__name {
@@ -110,7 +113,7 @@ export function MoviePosterCard(data) {
                font-size: 0.75rem
           }
 
-          .movie-card.explore-all span {
+          .movie-card span {
                position: absolute;
                top: 0;
                left: 0;
